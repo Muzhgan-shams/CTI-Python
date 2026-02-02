4c915dbf7eed994d570fc3ffcf0acae4d7a73de2
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-02-03 00:51:10 +0500

    fix: change elif with else to match remaining  scenarios

--- task008.py
+++ task008.py
@@ -28,16 +28,14 @@ user = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors: "))
 if user >= 0 and user <= 2:
     computer = random.randint(0, 2)
-    print(f"Your chocie: {arts[user]}")
+    print(f"Your choice: {arts[user]}")
     print(f"Computer choice: {arts[computer]}")
 
     if user == computer:
         print(f"{choices[user]} and {choices[computer]}\nIt is a draw 🤝")
     elif (user == 0 and computer == 2) or (user == 2 and computer == 1) or (user == 1 and computer == 0):
         print(f"{choices[user]} beats {choices[computer]}\nYou Win🏆🎉")
-    elif user < computer:
-        print(f"{choices[computer]} beats {choices[user]}\nYou Lose❌😭")
     else:
-        print("Invalid choice. Please select 0, 1, or 2.")
+        print(f"{choices[computer]} beats {choices[user]}\nYou Lose❌😭")
 else:
-    print("Please select from 0 to 2")
+    print("Invalid choice. Please select 0, 1, or 2.")
