b1aec548e4f421d5e56ada249a23ed21a4ca6956
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-04-04 23:50:39 +0500

    feat(secret_auction): add get_highest_bid fuction

--- secret_auction/auction.py
+++ secret_auction/auction.py
@@ -1,8 +1,28 @@
 # Secret Auction
-should_continue = True
+continue_bidding = True
 bids_db = {}
-while should_continue:
+
+
+def get_highest_bid(bids):
+    winner = ""
+    highest_bid = 0
+    for bidder in bids:
+        bid_amount = bids[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the bid of ${highest_bid}")
+
+
+while continue_bidding:
     name = input("What is your name? ")
     price = int(input("What is your bidding amount? "))
     bids_db[name] = price
-    should_continue = False
+    should_continue = input(
+        "Are there any bidders? Type 'Yes' or 'No'\n").lower().strip()
+    if should_continue == "no":
+        print()
+        continue_bidding = False
+        get_highest_bid(bids_db)
+    elif should_continue == "yes":
+        print("\n"*30)
