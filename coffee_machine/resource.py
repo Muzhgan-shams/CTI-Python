bb27e456bfd3b91accfddd2eb07c545a798e7c31
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-04-20 23:53:56 +0500

    feat(coffee_machine): add menu and resources for coffee machine

---
+++ coffee_machine/resource.py
@@ -0,0 +1,32 @@
+MENU = {
+    "Espresso": {
+        "Ingredients": {
+            "Water": 50,
+            "Coffee": 18
+        },
+        "Cost": 1.50
+    },
+    "Latte": {
+        "Ingredients": {
+            "Water": 200,
+            "Coffee": 24,
+            "Milk": 150
+        },
+        "Cost": 2.50
+    },
+    "Cappuccino": {
+        "Ingredients": {
+            "Water": 250,
+            "Coffee": 24,
+            "Milk": 100
+        },
+        "Cost": 3.00
+    },
+
+}
+
+resources = {
+    "Water": 300,
+    "Coffee": 100,
+    "Milk": 200
+}
